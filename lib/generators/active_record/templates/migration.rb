6ca8e1c098f372587a204b97d53d3802208b816e
chenqh <chenqh@cimutech.com>
2014-03-18 10:01:35 +0800

    fix bug, name error

--- lib/generators/active_record/templates/migration.rb
+++ lib/generators/active_record/templates/migration.rb
@@ -2,7 +2,7 @@ class ModelLocalizerCreate<%= table_name.camelize %> < ActiveRecord::Migration
   def change
     create_table(:<%= table_name %>) do |t|
       t.references :localizable, :polymorphic => true
-      t.string :collum_name
+      t.string :column_name
       t.string :locale
       t.string :value
 
@@ -10,7 +10,7 @@ class ModelLocalizerCreate<%= table_name.camelize %> < ActiveRecord::Migration
     end
 
     add_index :<%= table_name %>,
-              [ :resource_type, :resource_id, :column_name, :locale ],
+              [ :localizable_type, :localizable_id, :column_name, :locale ],
               :unique => true,
               :name => "localizer_ref_index"
   end
